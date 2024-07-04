da0ad70600790b474a067bfd50780035c08b0b71
Benedikt Kießling <bene@the-side.de>
2024-07-04 15:30:59 +0200

    create data

--- crud_demo.py
+++ crud_demo.py
@@ -26,11 +26,30 @@ def create_table(conn):
     conn.commit() 
     print('created a table')
 
+def create_data(conn):    
+    cursor = conn.cursor()
+    print('inserting data...')
+    for i in range(1,6):
+        insert_sql = ("INSERT INTO product "
+                   "(name, stock, price, created) "
+                   "VALUES(?, ?, ?, ?)")
+
+        params = ("product " + str(i), 3+i*4,
+                        0.4+i*8, datetime.now())
+        cursor.execute(insert_sql, params)
+        product_id = cursor.lastrowid
+        print('inserted with id=', product_id)
+
+    conn.commit()
+    cursor.close()
+    print('done')
+
 # open data
 conn = open_database()
 
 # creating table demo
-create_table(conn)
+#create_table(conn)
+create_data(conn)
 
 # close data
 conn.close()
